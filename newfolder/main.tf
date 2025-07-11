b4ef63871f96289a74d9a148a49c79136449a77b
ayushsaxena814 <ayushsaxena814@gmail.com>
2025-07-11 21:19:21 +0530

    this is new file

--- newfolder/main.tf
+++ newfolder/main.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "testing" {
   location = "central india"
   
 }
+
+resource "azurerm_storage_account" "testing-stg" {
+  name = "jghosrh"
+  location = "central india"
+  resource_group_name = "azurerm-rg"
+  account_replication_type = "GRS"
+  account_tier = "Standard"
+  
+}
