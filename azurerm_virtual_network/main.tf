421853fa6951bf33859d64b0b694c61aa9b379e7
Ayushmann814 <106014425+Ayushmann814@users.noreply.github.com>
2025-07-22 19:30:23 +0530

    Update main.tf
    i have added values of variable here only in the main.tf file

--- azurerm_virtual_network/main.tf
+++ azurerm_virtual_network/main.tf
@@ -4,3 +4,8 @@ resource "azurerm_virtual_network" "virtual_network" {
     resource_group_name = var.resource_group
     address_space = ["10.0.0.0/16"]
 }
+
+
+virtual_network_name = "frontend-rg-01"
+virtual_network_location = "central india"
+resource_group = "frontend-rg"
